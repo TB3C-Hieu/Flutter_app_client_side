393534b3b2840438c171da8880f3bd56909e0107
TB3C-Hieu <55200386+TB3C-Hieu@users.noreply.github.com>
2020-12-14 23:55:58 +0700

    Landing Page

---
+++ lib/Models/destinations.dart
@@ -0,0 +1,69 @@
+import 'active_model.dart';
+
+class Destination {
+  String imageUrl;
+  String city;
+  String country;
+  String description;
+  List<Activity> activities;
+
+  Destination({
+    this.imageUrl,
+    this.city,
+    this.country,
+    this.description,
+    this.activities,
+  });
+}
+
+List<Activity> activities = [
+  Activity(
+    imageUrl: 'assets/images/stmarksbasilica.jpg',
+    name: 'St. Mark\'s Basilica',
+    type: 'Sightseeing Tour',
+    startTimes: ['9:00 am', '11:00 am'],
+    rating: 5,
+    price: 30,
+  ),
+  Activity(
+    imageUrl: 'assets/images/gondola.jpg',
+    name: 'Walking Tour and Gonadola Ride',
+    type: 'Sightseeing Tour',
+    startTimes: ['11:00 pm', '1:00 pm'],
+    rating: 4,
+    price: 210,
+  ),
+  Activity(
+    imageUrl: 'assets/images/murano.jpg',
+    name: 'Murano and Burano Tour',
+    type: 'Sightseeing Tour',
+    startTimes: ['12:30 pm', '2:00 pm'],
+    rating: 3,
+    price: 125,
+  ),
+];
+
+List<Destination> destinations = [
+  Destination(
+    imageUrl: 'assets/images/NhaTrang.jpg',
+    city: 'NhaTrang',
+    country: 'VietNam',
+    description: 'Visit NhaTrang for an amazing and unforgettable adventure.',
+    activities: activities,
+  ),
+  Destination(
+    imageUrl: 'assets/images/PhuYen.jpg',
+    city: 'PhuYen',
+    country: 'VietNam',
+    description: 'Visit PhuYen for an amazing and unforgettable adventure.',
+    activities: activities,
+  ),
+  Destination(
+    imageUrl: 'assets/images/QuyNhon.jpg',
+    city: 'QuyNhon',
+    country: 'VietNam',
+    description: 'Visit QuyNhon for an amazing and unforgettable adventure.',
+    activities: activities,
+  ),
+
+];
